930300a043f323c67d87fc3a308243ee9ff8fe78
LionelMessiKB100 <99144412+pradolionel@users.noreply.github.com>
2023-02-09 21:49:19 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -93,7 +93,7 @@ class Main extends Sprite
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
-		addChild(fpsVar);
+		//addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
 		if(fpsVar != null) {
